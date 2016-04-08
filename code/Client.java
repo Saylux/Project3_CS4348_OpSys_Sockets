b9bf766b124dd9a03843836516a9d6f3d44446cf
Nicholas Powell <Salohcin22@gmail.com>
2016-04-08 12:46:03 -0500

    I got most of the functionality, threads, and the menu but I didn't write the menu in the correct format. I'm going to do the functionality of the menu and get the menu to the correct format later. also to run server do "make s" and to run client do "make c"

--- code/Client.java
+++ code/Client.java
@@ -1,6 +1,67 @@
+import java.io.*;
+import java.util.*;
+import java.net.Socket;
+
 public class Client
 {
-    public static void main(String args[])
+    public static void main(String args[]) throws IOException
     {
+	int port = Integer.parseInt(args[1]);//3010;
+	String addr = args[0]; //"cs1.utdallas.edu";
+	Socket client = new Socket(addr, port);
+	InputStreamReader inputStream = new InputStreamReader(client.getInputStream());
+
+	BufferedReader input = new BufferedReader(inputStream);
+	String nextLine = input.readLine();
+	//System.out.println(nextLine);
+	System.out.print("Enter in your name >> ");
+	Scanner scan = new Scanner(System.in);
+	String name = scan.nextLine();
+	PrintStream print = new PrintStream(client.getOutputStream());
+
+	print.println(name);
+	print.flush();
+	boolean exit = false;
+	while(!exit)
+	{
+	    System.out.println("//////////////////////////////");
+	    System.out.println("//The Following numbers are selectable Menu Choices");
+	    System.out.println("1 : Display the names of all known users");
+	    System.out.println("2 : Display the names of all currently connected users");
+	    System.out.println("3 : Send a text message to a particular user");
+	    System.out.println("4 : Send a text message to all currently connected users");
+	    System.out.println("5 : Send a text message to all known users");
+	    System.out.println("6 : Get my Messages");
+	    System.out.println("7 : Exit");
+	    System.out.println("//////////////////////////////");
+
+	    System.out.print("Enter in a number above >> ");
+	    String choiceStr = scan.nextLine();
+	    print.println(choiceStr);
+	    print.flush();
+	    int choice = Integer.parseInt(choiceStr);
+	    switch(choice)
+	    {
+	    case 1:
+		break;
+	    case 2:
+		break;
+	    case 3:
+		break;
+	    case 4:
+		break;
+	    case 5:
+		break;
+	    case 6:
+		break;
+	    case 7:
+		exit = true;
+		break;
+	    default:
+		System.out.println("ERROR: Enter in only Menu numbers 1-7.");
+		break;
+	    }
+	}
+	client.close();
     }
 }
--- code/Server.java
+++ code/Server.java
@@ -1,6 +1,98 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.*;
+import java.util.*;
+
 public class Server
 {
-    public static void main(String args[])
+    public static ArrayList connectedUsers = new ArrayList();
+    public class smallServ implements Runnable
     {
+	Socket localClient;
+	smallServ(Socket cli)
+	{
+	    localClient = cli;
+	}
+	
+	public void run()
+	{
+	    try
+	    {
+		InputStreamReader inputStream = new InputStreamReader(localClient.getInputStream());
+		BufferedReader input = new BufferedReader(inputStream);
+		PrintStream print = new PrintStream(localClient.getOutputStream());
+	
+		print.println("Server is Responding");
+		print.flush();
+	
+		String name = input.readLine();
+		
+		boolean exit = false;
+		while(!exit)
+		{
+		    int choice = 0;
+		    try
+		    {
+			choice = Integer.parseInt(input.readLine().trim());
+		    }
+		    catch(NumberFormatException e){}
+		    //System.out.println(choice);
+		    switch (choice)
+		    {
+		    case 1:
+			break;
+		    case 2:
+			break;
+		    case 3:
+			break;
+		    case 4:
+			break;
+		    case 5:
+			break;
+		    case 6:
+			break;
+		    case 7:
+			exit = true;
+			break;
+		    default:
+			System.out.println("ERROR: User gave bad entry (Not 1-7)");
+			break;
+		    }
+		}
+		//localClient.close();
+	    }
+	    catch(IOException e){}
+	    
+	}
+	
+    }
+    public static void main(String args[]) throws IOException
+    {
+	int port = Integer.parseInt(args[0]);
+	//String addr = "cs1.utdallas.edu";
+	//String name = "";
+	Server s = new Server();
+	ServerSocket server = new ServerSocket(port);
+	while(true)
+	{
+	    Socket client = server.accept();
+	
+	    smallServ serverInstance = s.new smallServ(client);
+	    Thread localServ = new Thread(serverInstance);
+	    localServ.start();
+	}
+	    /*InputStreamReader inputStream = new InputStreamReader(client.getInputStream());
+	BufferedReader input = new BufferedReader(inputStream);
+	PrintStream print = new PrintStream(client.getOutputStream());
+	
+	print.println("Server is Responding");
+	print.flush();
+	
+	name = input.readLine();
+       	
+	String choice = input.readLine();
+	System.out.println(choice);
+	client.close();
+	*/
     }
 }
